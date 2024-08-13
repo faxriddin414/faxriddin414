525d35e1c5973ff787782511b464b3a1b277451a
faxriddin414 <mfaxriddin46@gmail.com>
2024-08-13 17:59:25 +0300

    index1

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//function calculateBmi(a ,b) {
+  //  console.log(a + b);
+//}
+
+
+//function calculateBmi(name ,weiht, height){
+//    let bmi = weiht / (height * height);
+//    console.log(name + "'s BMI is ' + bmi");
+//    console.log(`${name}'s BMI is ${bmi}`)
+//}
+
+//calculateBmi("usmon" , 70, 1.8);
+//calculateBmi("absulloh" , 80, 1.7);
+//calculateBmi("faxriddin" , 60, 1.6);
+//calculateBmi("bobur" , 50, 1.5);
+//calculateBmi("latif" , 40, 1.4);
+
+
+//function calculateBmi(){
+//    const name = document.getElementById("name");
+//    const weightVal = document.getElementById("wight");
+//    const heightVal = document.getElementById("height");
+//    const result =document.querySelector("#result");
+    //perseFloat --> 3.14 xammasini saqlab qoladi  *pareInt --> 3 xammasini olib tashidi :)
+//    const weight = parseFloat(weightVal.value);
+//    const height = parseFloat(heightVal.value);
+
+//    let bmi = (weight / (height * height)).toFixed(2);
+    
+  //  result.textContent = `${name.value}'s BMI is ${bmi} `;
+//}
+
+let x = 10;
+let y = 20;
+let z = x +y;
+document.getElementById("demo").innerHTML = z;
